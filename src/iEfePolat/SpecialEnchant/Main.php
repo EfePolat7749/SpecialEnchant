5eaaf9234b0ef5674aea3dfdbdf3cbe46dff2ca1
EfePolat7749 <64210911+EfePolat7749@users.noreply.github.com>
2021-03-04 21:07:03 +0300

    Update Main.php

--- src/iEfePolat/SpecialEnchant/Main.php
+++ src/iEfePolat/SpecialEnchant/Main.php
@@ -1,1 +1,40 @@
+<?php
 
+declare(strict_types=1);
+
+namespace iEfePolat\SpecialEnchant;
+
+use pocketmine\{
+        plugin\PluginBase,
+        command\Command,
+        command\CommandSender,
+        Player,
+        Server,
+        event\Listener
+};
+use iEfePolat\SpecialEnchant\{
+        commands\SpecialEnchantCommand,
+        events\SpecialEnchantEvent
+};
+class Main extends PluginBase implements Listener{
+        
+        private static $api;
+                
+       public function onLoad(){
+          $this->registerMultipleAcces();
+        }
+                
+        public function registerMultipleAcces(){          
+                return static::$api = $this;
+        }
+        
+        public function onEnable(){
+              $this->getServer()->getPluginManager()->registerEvents(new SpecialEnchantEvent, $this);
+                $this->getServer()->getCommandMap()->register("ozelbuyu", new SpecialEnchantCommand($this)); //tabiki
+        }
+        public static function getAPI(): ?self{
+                return self::$api;
+        }
+                  
+
+}
